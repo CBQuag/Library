f36a86e35fdb1ced2882dd51f58de6b58b63463e
Conrad Quagliaroli <conradbquag@gmail.com>
2023-05-31 09:15:41 -0400

    added more tests

--- Library.js
+++ Library.js
@@ -3,5 +3,12 @@ class Library{
     constructor() {
         this.inventory = [];
     }
-    
+    showInventory(){
+        return this.inventory;
+    }
+    addBooks(books) {
+        this.inventory.push(...books)
+    }
 }
+
+module.exports=Library
--- Library.test.js
+++ Library.test.js
@@ -4,13 +4,26 @@ describe('Vending machine', () => {
 
     let library;
 
-    beforeEach((=> {
+    let books=[{name:'Underground', author: 'Haruki Murakami'}]
+
+    beforeEach(()=> {
         library = new Library();
-    }))
+    })
     
     test('should be able to list inventory', () => { 
-        expect(library.showInventory)
+        expect(library.showInventory()).toEqual([]);
 
+    })
+    
+    test('should be able to add books', () => { 
+        library.addBooks(books)
+        expect(library.showInventory()).toEqual(books)
+    })
+    
+    test('should be able to issue out books', () => { 
+        library.addBooks(books)
+        library.issueBook('Underground');
+        expect(library.showInventory()).toEqual([])
      })
 
 })
